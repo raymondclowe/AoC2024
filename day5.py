e2bcda6e0454ce4bf19c405af7855179cdcdf512
Raymond Lowe <raymondclowe@gmail.com>
2024-12-06 08:46:44 +0800

    d5p1 ok

--- day5.py
+++ day5.py
@@ -1,4 +1,4 @@
-with open("day5-example.txt") as f:
+with open("day5.txt") as f:
     lines = f.readlines()
 
 rules = []
@@ -7,14 +7,44 @@ updates = []
 switch = False
 
 for line in lines:
+    print(line)
     if line == '\n':
         switch = True
         continue
-    if switch:
-        updates.append(line.strip())
+    if not switch:
+        rule_lst = [int(x) for x in line.strip().split('|')]
+        rules.append(rule_lst)
     else:
-        rules.append( [int(x) for x in line.strip().split(',')])
+        updates.append( [int(x) for x in line.strip().split(',')])
 
 print(rules)
 print(updates)
 print ('done')
+
+def conforms_to_rule(update, rule):
+    
+    if not ((rule[0] in update) and (rule[1] in update)):
+        return True
+    
+    pos_left_part = update.index(rule[0])
+    pos_right_part = update.index(rule[1])
+    if pos_left_part > pos_right_part:
+        return False
+    return True
+
+def  update_valid(update):
+    for rule in rules:
+        if not conforms_to_rule(update, rule):
+            return False
+    return True
+
+total = 0
+for update in updates:
+    if update_valid(update):
+        print(f'valid: {update}')
+        middle_value_of_update = update[len(update)//2]
+        total += middle_value_of_update
+    else:
+        print(f'invalid: {update}')
+
+print(f'total: {total}')
